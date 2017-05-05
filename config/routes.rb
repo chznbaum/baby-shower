41522ca4b703def297ef8992de47e23daa1f5c63
Chazona <chznbaum@gmail.com>
2017-05-04 22:15:52 -0400

    configured routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
-  get 'pages/help'
+  get 'help', to: 'pages#help'
 
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
+
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
