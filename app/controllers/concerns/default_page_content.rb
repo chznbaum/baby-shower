ef17d04e10288bd4c1c7a409fe9f8e52719c1ad6
Chazona <chznbaum@gmail.com>
2017-05-06 13:57:50 -0400

    refactored default mom variable to the user with mom role instead of string

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,8 +6,8 @@ module DefaultPageContent
     end
 
     def set_page_defaults
-        @mother_to_be = "Jane Doe"
-        @page_title = "#{@mother_to_be}'s Baby Shower Website"
-        @seo_keywords = "baby shower pregnancy event family"
+        @mother_to_be = User.where(roles: "mom").order("created_at DESC").first
+        @page_title = "#{@mother_to_be.name}'s Baby Shower Website"
+        @seo_keywords = "#{@mother_to_be.name} baby shower pregnancy event family"
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
     end
 
     def copyright_generator
-        @copyright = ChznbaumViewTool::Renderer.copyright @mother_to_be, 'All rights reserved'
+        @copyright = ChznbaumViewTool::Renderer.copyright @mother_to_be.name, 'All rights reserved'
     end
 end
