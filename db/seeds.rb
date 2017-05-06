cae2a8bf7094685516d2cdbd23c8015727f1128f
Chazona <chznbaum@gmail.com>
2017-05-05 20:37:53 -0400

    reseeded database with sample users-updates relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,57 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Users
+User.create!(name: "Admin User",
+             email: "admin@example.com",
+             password: "passwordpassword",
+             roles: "admin")
+
+User.create!(name: "Helper User",
+             email: "helper@example.com",
+             password: "passwordpassword",
+             roles: "helper")
+
+User.create!(name: "Second Helper",
+             email: "helper2@example.com",
+             password: "passwordpassword",
+             roles: "helper")
+
+User.create!(name: "Example User",
+             email: "example@example.com",
+             password: "passwordpassword",
+             roles: "helper")
+
+User.create!(name: "Mom User",
+             email: "mom@example.com",
+             password: "passwordpassword",
+             roles: "mom")
+
+User.create!(name: "Regular User",
+             email: "regular@example.com",
+             password: "passwordpassword",
+             roles: "user")
+
+User.create!(name: "Second Regular",
+             email: "regular2@example.com",
+             password: "passwordpassword",
+             roles: "user")
+
+puts "#{User.count} users created."
+
 # Updates
+
 10.times do |x|
     3.times do |update|
         Update.create!(
-            body: Faker::Lorem.words(20).join(" ")
+            body: Faker::Lorem.words(20).join(" "),
+            user_id: rand(1..5)
         )
     end
     Update.create!(
         body: Faker::Lorem.words(15).join(" "),
-        image: "http://placehold.it/800x400"
+        image: "http://placehold.it/800x400",
+        user_id: rand(1..5)
     )
 end
 
