ef42c7c11814ebb61666e9889a4d46e5aaff01f6
Chazona <chznbaum@gmail.com>
2017-05-08 23:01:07 -0400

    implemented links to events and next upcoming event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @event = Event.order(:event_datetime).first
   end
 
   def about
     @page_title = "About #{@mother_to_be.name}"
+    @event = Event.order(:event_datetime).first
   end
 
   def help
