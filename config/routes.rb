b4affe34227b92d7b12690fe1a8b127dd0b9501f
Chazona <chznbaum@gmail.com>
2017-05-08 22:45:32 -0400

    implemented view of comments on event show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :users, only: [:index, :show]
-  resources :events
+  resources :events do
+    resources :comments
+  end
   resources :updates do
     resources :comments
     resources :likes
