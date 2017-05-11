5ba44f1a92e7f265d11d8afcae02d0938af1e223
Chazona <chznbaum@gmail.com>
2017-05-10 21:53:40 -0400

    implemented partials for nav and footer

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,6 +7,7 @@ module DefaultPageContent
 
     def set_page_defaults
         @mother_to_be = User.where(roles: "mom").order("created_at DESC").first
+        @site_title = "#{@mother_to_be.name}'s Baby Shower"
         @page_title = "#{@mother_to_be.name}'s Baby Shower Website"
         @seo_keywords = "#{@mother_to_be.name} baby shower pregnancy event family"
     end
